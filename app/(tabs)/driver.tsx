3b1d5aa9b2ed30a24bf3bdce7845690396656590
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-11-25 18:19:40 -0800

    added starter code to driver.tsx and organized tabs in layout.tsx

---
+++ app/(tabs)/driver.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function DriverScreen() {
+  return (
+    <View>
+      <Text>Driver Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
