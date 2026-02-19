623076b56f2ab3bae4652817e867b854cd77f843
ChrisK15 <chris.kelamyan.115@gmail.com>
2026-02-19 13:25:22 -0800

    updated models.ts to add new claimed status and supporting stuff

--- types/models.ts
+++ types/models.ts
@@ -16,6 +16,7 @@ export interface Driver {
 	vehicleInfo: VehicleInfo;
 	documents?: DriverDocuments;
 	currentLocation: Location;
+	geohash: string;
 	serviceRadius: number;
 	rating?: number;
 	totalTrips: number;
@@ -48,8 +49,11 @@ export interface Request {
 	pickupAddress: string;
 	dropoffAddress: string;
 	serviceType: 'tow';
-	status: 'searching' | 'matched' | 'accepted' | 'cancelled';
+	status: 'searching' | 'claimed' | 'matched' | 'accepted' | 'cancelled';
 	matchedDriverId?: string; // only exists after being matched
+	claimedByDriverId?: string;
+	claimExpiresAt?: Date;
+	notifiedDriverIds?: string[];
 	createdAt: Date;
 	expiresAt: Date;
 	commuterName?: string;
