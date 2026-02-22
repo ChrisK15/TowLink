6206217a0ccb59cc24cf00e54f72c614ebd6ed20
ChrisK15 <chris.kelamyan.115@gmail.com>
2026-02-21 21:47:55 -0800

    fixed bugs from review. ready to merge

--- services/requestCalculations.ts
+++ services/requestCalculations.ts
@@ -1,15 +1,18 @@
 import { Location, Request } from '@/types/models';
 import { getDistanceInKm, kmToMiles } from './geoLocationUtils';
 
-export function calculateDistanceMiles(point1: Location, point2: Location) {
+export function calculateDistanceMiles(
+	point1: Location,
+	point2: Location,
+): number {
 	return Math.round(kmToMiles(getDistanceInKm(point1, point2)) * 10) / 10;
 }
 
-export function calculateETA(distanceMiles: number) {
+export function calculateETA(distanceMiles: number): number {
 	return Math.ceil((distanceMiles / 25) * 60); // minutes
 }
 
-export function calculateFare(tripDistanceMiles: number) {
+export function calculateFare(tripDistanceMiles: number): number {
 	return Math.max(65, Math.round(50 + 5 * tripDistanceMiles));
 }
 
@@ -17,6 +20,7 @@ export function enrichRequestWithCalculations(
 	request: Request,
 	driverLocation: Location,
 ): Request {
+	if (!request.dropoffLocation) return request;
 	const estimatedPickupDistance = calculateDistanceMiles(
 		driverLocation,
 		request.location,
@@ -25,7 +29,8 @@ export function enrichRequestWithCalculations(
 		request.location,
 		request.dropoffLocation,
 	);
-	const totalJobDistance = estimatedPickupDistance + totalTripDistance;
+	const totalJobDistance =
+		Math.round((estimatedPickupDistance + totalTripDistance) * 10) / 10;
 	const estimatedETA = calculateETA(estimatedPickupDistance);
 	const estimatedPrice = calculateFare(totalTripDistance);
 
