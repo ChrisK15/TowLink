9902168b0254af230cb56d553ff32cba8621486e
ChrisK15 <chris.kelamyan.115@gmail.com>
2026-02-21 20:36:27 -0800

    requestCalculations.ts now returns a new request object with updated values

--- services/requestCalculations.ts
+++ services/requestCalculations.ts
@@ -1,4 +1,4 @@
-import { Location } from '@/types/models';
+import { Location, Request } from '@/types/models';
 import { getDistanceInKm, kmToMiles } from './geoLocationUtils';
 
 export function calculateDistanceMiles(point1: Location, point2: Location) {
@@ -7,4 +7,32 @@ export function calculateDistanceMiles(point1: Location, point2: Location) {
 
 export function calculateETA(distanceMiles: number) {
 	return Math.ceil((distanceMiles / 25) * 60); // minutes
+}
+
+export function calculateFare(tripDistanceMiles: number) {
+	return Math.max(65, 50 + 5 * tripDistanceMiles);
+}
+
+export function enrichRequestWithCalculations(
+	request: Request,
+	driverLocation: Location,
+): Request {
+	const estimatedPickupDistance = calculateDistanceMiles(
+		driverLocation,
+		request.location,
+	);
+	const totalTripDistance = calculateDistanceMiles(
+		request.location,
+		request.dropoffLocation,
+	);
+	const estimatedETA = calculateETA(estimatedPickupDistance);
+	const estimatedPrice = calculateFare(totalTripDistance);
+
+	return {
+		...request,
+		estimatedPickupDistance,
+		totalTripDistance,
+		estimatedETA,
+		estimatedPrice,
+	};
 }
